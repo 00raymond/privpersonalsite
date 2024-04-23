9ec7ca8a8008126c5d037775983ed86ff19442c6
Raymond House <raymondhouse2003@gmail.com>
2024-04-22 19:55:03 -0700

    moved

---
+++ src/components/Experience.tsx
@@ -0,0 +1,65 @@
+import '@/components/pagestyles.css'
+
+export const Experience = () => {
+    return (
+        <div className=" to-slate-950 rounded-xl border border-slate-800 bg-gradient-to-r from-slate-900 px-8 py-10 shadow-2xl text-left max-w-4xl w-full max-h-[700px] overflow-y-auto">
+            <ol className=" relative border-s border-gray-200 dark:border-gray-700">
+                <li className="mb-10 ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">July 2023 - Present</time>
+                    <div className={"flex space-x-1 items-baseline"}>
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Android Developer Researcher,</h3>
+                        <p className="text-md font-normal italic text-gray-900 dark:text-gray-300">U.C. Irvine School of Medicine</p>
+                    </div>
+                    <p className=" font-normal text-gray-500 dark:text-gray-400">Worked on a machine-learning based Android app which identifies eyedrop medication labels using object detection and text recognition.</p>
+                    <p className=" font-normal text-gray-500 dark:text-gray-400">Wrote methods to extract expiration date from medicine bottles and wrote utility methods to store and retrieve JSON parsed data in internal storage.</p>
+                    <p className=" font-bold text-gray-500 dark:text-gray-400 language-gradient">Android Studio, Java, Kotlin, ML Kit, Gson, Mobile Dev</p>
+                </li>
+                <li className="mb-10 ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 2023 - January 2024</time>
+                    <div className={"flex space-x-1 items-baseline"}>
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Software Engineer Intern,</h3>
+                        <p className="text-md font-normal italic text-gray-900 dark:text-gray-300">NeuroLeap Corp.</p>
+                    </div>
+                    <p className=" font-normal text-gray-500 dark:text-gray-400">Developed API endpoints for the company main site in a REST architecture for email transactions and user information changes, utilizing JWT authentication.</p>
+                    <p className=" font-normal text-gray-500 dark:text-gray-400">Wrote several HTML templates for email transactions and conducted API/Db testing.</p>
+                    <p className=" font-bold text-gray-500 dark:text-gray-400 language-gradient">VSCode, Golang, Vue, Javascript, PostgresQL, Postman, Web Dev</p>
+                </li>
+                <li className="mb-10 ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2023</time>
+                    <div className={"flex space-x-1 items-baseline"}>
+                        <h3 className="text-md font-semibold text-gray-900 dark:text-white">Air Quality Info iOS App</h3>
+                        <p className="text-md font-normal italic text-gray-900 dark:text-gray-300">Hackathon</p>
+                    </div>
+                    <p>Private Repo </p>
+                    <p className="font-bold text-gray-500 dark:text-gray-400 language-gradient">
+                        Xcode, Swift, SwiftUI
+                    </p>
+                </li>
+                <li className="mb-10 ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2022</time>
+                    <div className={"flex space-x-1 items-baseline"}>
+                        <h3 className="text-md font-semibold text-gray-900 dark:text-white">Pomopal Chrome Extension</h3>
+                        <p className="text-md font-normal italic text-gray-900 dark:text-gray-300">Hackathon</p>
+                    </div>
+                    <a href="https://github.com/00raymond/pomopal" target="_blank" rel="noopener noreferrer" className="text-blue-500 dark:text-blue-400">Repo Link</a>
+                    <p className="font-bold text-gray-500 dark:text-gray-400 language-gradient">
+                        Javascript, HTML, CSS
+                    </p>
+                </li>
+                <li className="mb-10 ms-4">
+                    <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                    <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 2021</time>
+                    <div className={"flex space-x-1 items-baseline"}>
+                        <h3 className="text-md font-semibold text-gray-900 dark:text-white">Enrolled in B.S. Software Engineering</h3>
+                        <p className="text-md font-normal italic text-gray-900 dark:text-gray-300"></p>
+                    </div>
+                    <p className=" font-bold text-gray-500 dark:text-gray-400 language-gradient">GPA: 3.8</p>
+                </li>
+            </ol>
+        </div>
+        )
+}
---
+++ src/components/nameplate.tsx
@@ -0,0 +1,49 @@
+import me from "../../../personalsite/src/assets/me.jpg";
+import resume from "../../../personalsite/src/assets/Raymond_House_resume.pdf";
+export const Nameplate = () => {
+
+    return (
+        <div className="to-slate-950 relative overflow-hidden rounded-xl border border-slate-800 bg-gradient-to-r from-slate-900 px-8 py-16 shadow-2xl">
+            <div className="pointer-events-none absolute -inset-px opacity-0 transition duration-300"/>
+                <div className={"flex space-x-3 items-center justify-center"}>
+                    <img src={me} alt="me" className="w-48 h-48 rounded-xl shadow-lg border-2 border-slate-800"/>
+                    <div className={"flex flex-col items-center justify-center"}>
+                        <p className="tracking-tight font-medium text-3xl text-slate-200 ">
+                            raymond house
+                        </p>
+                        <div className={"text-xl mt-2"}>
+                            <p>software developer</p>
+                            <p>uc irvine 2025</p>
+                        </div>
+                        <a
+                            href={resume} target="_blank" rel="noopener noreferrer"
+                            className={"hover:bg-slate-900 transition-all duration-200 items-center justify-center text-2xl mt-1 flex space-x-1 bg-black bg-opacity-25 border-2 border-slate-800 p-0.5 pt-0 pl-4 pr-4 rounded-xl"}>
+                            <p>resume</p>
+                            <svg className="mt-1.5 w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"/>
+                            </svg>
+                        </a>
+                        <div className={"flex items-center justify-center space-x-2 mt-1"}>
+                            <a href="https://github.com/00raymond" target="_blank" rel="noopener noreferrer">
+                                <svg className="w-12 h-12 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"/>
+                                </svg>
+                            </a>
+                            <a href="https://linkedin.com/in/raymondbhouse" target="_blank" rel="noopener noreferrer">
+                                <svg className="w-12 h-12 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clip-rule="evenodd"/>
+                                    <path d="M7.2 8.809H4V19.5h3.2V8.809Z"/>
+                                </svg>
+                            </a>
+                            <a href="mailto:raymondhouse2003@gmail.com" target="_blank" rel="noopener noreferrer">
+                                <svg className="w-12 h-12 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path d="M2.038 5.61A2.01 2.01 0 0 0 2 6v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6c0-.12-.01-.238-.03-.352l-.866.65-7.89 6.032a2 2 0 0 1-2.429 0L2.884 6.288l-.846-.677Z"/>
+                                    <path d="M20.677 4.117A1.996 1.996 0 0 0 20 4H4c-.225 0-.44.037-.642.105l.758.607L12 10.742 19.9 4.7l.777-.583Z"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    );
+};
